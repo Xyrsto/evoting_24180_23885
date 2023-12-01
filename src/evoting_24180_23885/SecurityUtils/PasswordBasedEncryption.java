2b4ad9e60763649257879e60a36b933de5a5c0a4
Xyrsto <73367973+Xyrsto@users.noreply.github.com>
2023-12-01 14:25:34 +0000

    Inicio da distribuição. Login está funcional

--- src/evoting_24180_23885/SecurityUtils/PasswordBasedEncryption.java
+++ src/evoting_24180_23885/SecurityUtils/PasswordBasedEncryption.java
@@ -14,24 +14,51 @@ import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
- *
- * @author rodri
+ * A classe PasswordBasedEncryption fornece métodos para realizar criptografia e descriptografia
+ * de dados usando um esquema de criptografia baseado em senha.
+ * Ela utiliza a cifra AES em modo CBC (Cipher Block Chaining) com preenchimento PKCS5Padding
+ * e a função de derivação de chave PBKDF2 para fortalecer a segurança da senha.
  */
 public class PasswordBasedEncryption {
     String data;
 
+    /**
+     * Construtor da classe PasswordBasedEncryption.
+     * 
+     * @param data Os dados a serem processados pela criptografia ou descriptografia.
+     */
     public PasswordBasedEncryption(String data) {
         this.data = data;
     }
 
+    /**
+     * Obtém os dados atualmente armazenados na instância.
+     * 
+     * @return Os dados armazenados.
+     */
     public String getData() {
         return data;
     }
 
+    
+    /**
+     * Define os dados a serem processados pela criptografia ou descriptografia.
+     * 
+     * @param data Os dados a serem definidos.
+     */
     public void setData(String data) {
         this.data = data;
     }
     
+    /**
+     * Cria e retorna uma instância da classe Cipher configurada para o modo especificado
+     * (criptografia ou descriptografia) utilizando um esquema de criptografia baseado em senha.
+     * 
+     * @param mode O modo da cifra (Cipher.ENCRYPT_MODE para criptografia, Cipher.DECRYPT_MODE para descriptografia).
+     * @param password A senha utilizada para derivar a chave.
+     * @return Uma instância da classe Cipher configurada para o modo especificado.
+     * @throws Exception Se ocorrer um erro durante a criação da instância Cipher.
+     */
     public static Cipher createPBE(int mode, String password) throws Exception
     {
         //generate salt - random 
@@ -58,11 +85,27 @@ public class PasswordBasedEncryption {
         return cipher;
     }
     
+    /**
+     * Criptografa os dados utilizando uma senha e retorna o resultado.
+     * 
+     * @param data Os dados a serem criptografados.
+     * @param password A senha utilizada para derivar a chave.
+     * @return Os dados criptografados.
+     * @throws Exception Se ocorrer um erro durante a operação de criptografia.
+     */
     public static byte[] encrypt(byte[] data, String password) throws Exception{
         Cipher cipher = createPBE(Cipher.ENCRYPT_MODE, password);
         return cipher.doFinal(data);
     }
     
+    /**
+     * Descriptografa os dados criptografados utilizando uma senha e retorna o resultado.
+     * 
+     * @param data Os dados criptografados a serem descriptografados.
+     * @param password A senha utilizada para derivar a chave.
+     * @return Os dados descriptografados.
+     * @throws Exception Se ocorrer um erro durante a operação de descriptografia.
+     */
     public static byte[] decrypt(byte[] data, String password) throws Exception{
         Cipher cipher = createPBE(Cipher.DECRYPT_MODE, password);
         return cipher.doFinal(data);
---
+++ src/evoting_distributed_24180_23885/Cliente/RemoteInterface.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package evoting_distributed_24180_23885.Cliente;
+
+import java.rmi.RemoteException;
+import java.rmi.Remote;
+
+/**
+ * A interface RemoteInterface define os métodos remotos disponíveis para autenticação de clientes
+ * em um sistema distribuído de votação eletrônica.
+ * Estende a interface Remote para indicar que os métodos podem ser chamados remotamente.
+ * 
+ */
+public interface RemoteInterface extends Remote {
+    /**
+     * Realiza a autenticação de um cliente no sistema.
+     * 
+     * @param numCC O número do Cartão de Cidadão do cliente.
+     * @param password A senha do cliente.
+     * @return O número do Cartão de Cidadão se a autenticação for bem-sucedida; caso contrário, retorna null.
+     * @throws RemoteException Exceção lançada em caso de erro remoto.
+     * @throws Exception Exceção genérica que pode ser lançada durante o processo de autenticação.
+     */
+    public String loginUser(String numCC, String password) throws RemoteException, Exception;
+}
